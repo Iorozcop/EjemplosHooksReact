ff1d17b863df5b0f15a1433d82e69c159fddf36c
Iorozcop <iorozcopuerto@gmail.com>
2021-04-29 14:56:22 +0200

    añadidas nuevas constantes en useEffect para comprobar su funcionamiento

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ export default function App() {
 
   const [count,setCount] = useState(0);
   const [words, setWords] = useState('');
+  const [persons, setPersons] = useState('');
 
+  const names =['Carlos','Isa','Jose'];
+  const addNames = () => names.map(name => name + ', ');
+  
   useEffect(()=>{
     if(count>0){
-      setWords('Yo aparezco solo cuando count cambia');
+      setWords('Yo aparezco solo cuando aparecen los nombres y count es >0');
     }
-  },[count]);
+  },[persons]);
+
+  useEffect(()=>{
+    setWords('yo aparezco siempre');  
+  },[]);
 
   return (
     <>
@@ -19,11 +27,13 @@ export default function App() {
       <p>He hecho click {count} veces</p>
       <button onClick={()=>setCount(count +1)}>sumar</button>
       <button onClick={()=>setCount(count -1)}>restar</button> 
+      <button onClick={()=>setPersons(addNames)}>Mostrar Nombres</button>
     </div>
 
     <div>
       <h1>Ejemplo useEffect</h1>
       <p>{words}</p>
+      <p>{persons}</p>
     </div>
     </>
   );
